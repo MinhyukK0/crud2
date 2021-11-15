4cfc6370261fc379d11617655b29b0b3a1368b91
minhyukko <komhgogo@gmail.com>
2021-11-15 20:32:44 +0900

    views.py post기능 추가

--- movie/views.py
+++ movie/views.py
@@ -21,10 +21,19 @@ class ActorView(View):
                     '이름'  : actor.first_name,
                     '성'   : actor.last_name,
                     '생일'  : actor.date_of_birth,
-                    '출연영화': [x.title for x in actor.movie.all()]
+                    '출연영화': [x.title for x in actor.movie.all()],
                 }
             )
         return JsonResponse({'results' : results}, status = 200)
+    # post
+    def post(self, request):
+        data = json.loads(request.body)
+        Actor.objects.create(
+            first_name = data['이름'],
+            last_name = data['성'],
+            date_of_birth = data['생일'],
+        )
+        return JsonResponse({'MESSAGE':'Created'},status = 201)
 
 # Movie
 class MovieView(View):
@@ -38,7 +47,16 @@ class MovieView(View):
                     'title'       : movie.title,
                     'release_date': movie.release_date,
                     'running_time': movie.running_time,
-                    'actor'       : [x.first_name for x in movie.actor_set.all()]
+                    'actor'       : [x.first_name for x in movie.actor_set.all()],
                 }
             )
         return JsonResponse({'results' : results}, status = 200)
+    # post
+    def post(self, request):
+        data = json.loads(request.body)
+        Movie.objects.create(
+            title = data['제목'],
+            release_date = data['개봉날짜'],
+            running_time = data['상영시간'],
+        )
+        return JsonResponse({'MESSAGE':'Created'},status = 201)
--- owner/urls.py
+++ owner/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from owner.views import OwnerView, DogView
 
 urlpatterns = [
-    path('owner', OwnerView.as_view()),
-    path('dog', DogView.as_view()),
+    re_path('owner', OwnerView.as_view()),
+    re_path('dog', DogView.as_view()),
 ]
